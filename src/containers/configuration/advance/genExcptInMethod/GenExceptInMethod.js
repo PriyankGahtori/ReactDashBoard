d1f8f77f5198af5e9b131f2518e4e6ba7e0dc24a
PriyankGahtori <priyank.gahtori@gmail.com>
2017-01-06 18:30:01 +0530

    Chnges for color of advance setting & close NDagent

--- src/containers/configuration/advance/genExcptInMethod/GenExceptInMethod.js
+++ src/containers/configuration/advance/genExcptInMethod/GenExceptInMethod.js
@@ -12,7 +12,7 @@ import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
 import Snackbar from 'material-ui/Snackbar';
 import FlatButton from 'material-ui/FlatButton';
-import {hashHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import TextField from 'material-ui/TextField';
 import { reduxForm } from 'redux-form';
 import _ from "lodash";
@@ -21,32 +21,32 @@ import ConfirmDialog from 'material-ui/Dialog';
 import DialogGenExceptInMethod from 'material-ui/Dialog';
 
 import * as validate from '../../../../actions/validateGeneralKeywords';
-import * as constructValue from '../../../utils/keywordsValue.js' ;
-import {triggerRunTimeChanges} from '../../../../actions/runTimeChanges';
-import * as actionCreators  from '../../../../actions/index';
+import * as constructValue from '../../../utils/keywordsValue.js';
+import { triggerRunTimeChanges } from '../../../../actions/runTimeChanges';
+import * as actionCreators from '../../../../actions/index';
 import Checkbox from '../../../../components/CheckboxWrapper';
-import {getKeywordsData,submitKeywordData}  from '../../../../actions/index';
+import { getKeywordsData, submitKeywordData } from '../../../../actions/index';
 import FormGenExcptInMethod from './Form_GenerateExceptionInMethod';
 import * as  modifiedVal from './ModifyValue';
 
 
 const styles = {
 	text: {
-		fontSize:18,
-		paddingLeft:6
+		fontSize: 18,
+		paddingLeft: 6
 	},
 
-	mainBlock:{
-		paddingLeft:10,
-		paddingBottom:20
+	mainBlock: {
+		paddingLeft: 10,
+		paddingBottom: 20
 	},
-	row1:{
-		paddingBottom:20
+	row1: {
+		paddingBottom: 20
 	},
-	row2:{
-		paddingTop : 40
+	row2: {
+		paddingTop: 40
 	},
-	title:{
+	title: {
 		fontSize: 16,
 		padding: 8
 	}
@@ -59,9 +59,8 @@ const styles = {
 
 
 const style = {
-  //margin: 20,
-  textAlign: 'center',
-  display: 'inline-block'
+	textAlign: 'center',
+	display: 'inline-block'
 };
 
 const NewButtonstyle = {
@@ -78,213 +77,204 @@ class GenExcptInMethod extends React.Component {
 
 	constructor(props) {
 		super(props);
-  //this.state ={enableBCIDebug:false}
-  this.state = {openEnableExcptCapturingDialog : false}
-  this.state = {disableAdvancedSettingTab1 :!this.props.getAllKeywordData.BCICapturingCheckBox}
-  this.state = {getAllKeywordData:this.props.getAllKeywordData}
-  this.state = {enableExcptCheckBox:false,genExcptInMethod:false}
-  this.state = {openSnackBar:false}
-}
-
-
-//this function is called first when component gets first loaded
-componentWillMount() {
-	this.state = {openSnackBar:false}
-}
-
-componentWillReceiveProps(nextProps)
-{
-	if(this.props.getAllKeywordData != nextProps.getAllKeywordData){
-		this.setState({getAllKeywordData : nextProps.getAllKeywordData,
-			genExcptInMethod : nextProps.getAllKeywordData.genExcptInMethod
-		});
+		this.state = { openEnableExcptCapturingDialog: false }
+		this.state = { disableAdvancedSettingTab1: !this.props.getAllKeywordData.BCICapturingCheckBox }
+		this.state = { getAllKeywordData: this.props.getAllKeywordData }
+		this.state = { enableExcptCheckBox: false, genExcptInMethod: false }
+		this.state = { openSnackBar: false }
 	}
 
-	if(this.props.getAllKeywordData.ExcptCapturingCheckBox != nextProps.getAllKeywordData.ExcptCapturingCheckBox)
-		this.setState({disableAdvancedSettingTab1:!nextProps.getAllKeywordData.ExcptCapturingCheckBox})
+	//this function is called first when component gets first loaded
+	componentWillMount() {
+		this.state = { openSnackBar: false }
+	}
 
-}
+	componentWillReceiveProps(nextProps) {
+		if (this.props.getAllKeywordData != nextProps.getAllKeywordData) {
+			this.setState({
+				getAllKeywordData: nextProps.getAllKeywordData,
+				genExcptInMethod: nextProps.getAllKeywordData.genExcptInMethod
+			});
+		}
 
+		if (this.props.getAllKeywordData.ExcptCapturingCheckBox != nextProps.getAllKeywordData.ExcptCapturingCheckBox)
+			this.setState({ disableAdvancedSettingTab1: !nextProps.getAllKeywordData.ExcptCapturingCheckBox })
 
-  /*
-  *  functions for enableBCICapturing Dialog
-  */
+	}
 
-  enableExcptCapturingDialog(){
-  	this.setState({openEnableExcptCapturingDialog:true});
-  }
+	/*
+	*  functions for enableBCICapturing Dialog
+	*/
 
+	enableExcptCapturingDialog() {
+		this.setState({ openEnableExcptCapturingDialog: true });
+	}
 
-  handleGenExcptInMethod(event,isInputChecked){
-  	if(isInputChecked === true ){
+	handleGenExcptInMethod(event, isInputChecked) {
+		if (isInputChecked === true) {
+			this.setState({ openSnackBar: true, genExcptInMethod: true })
+			var data = this.props.getAllKeywordData.data;
+			this.props.genExcptInMethod(true);
+		}
+		else {
+			this.setState({ openCnfrmDisbleDialog: true })
 
-      //this.props.setDefValBCICapturingKeywords();
-      this.setState({openSnackBar:true,genExcptInMethod:true})
-      var data = this.props.getAllKeywordData.data ;
-      //this.submitForm(data);
-       //this.props.setDefValBCICapturingKeywords();
-       this.props.genExcptInMethod(true);
-   }
-   else{
-   	this.setState({openCnfrmDisbleDialog:true})
-   	 
-   }
-}
+		}
+	}
 
-handleCancelGenExcptInMethod(){
-	this.setState({openEnableExcptCapturingDialog:false});
-}
-
-
-handleSubmitGenExcptInMethod(){
-	this.refs.enableGenExcptInMethod.submit();
-}
-
-
-
-handleRequestClose(){
-	this.setState({openSnackBar:false
-	})
-}
-
-/*
-* Disable Dialog functions
-*/
-cnfrmDisableExcptVal(){
-	var data = {'generateExceptionInMethod':'0'}
-	this.submitForm(data);
-	this.setState({ openCnfrmDisbleDialog:false
-	})
-
-}
-
-handleCancelDisableExcptVal(){
-	this.setState({ openCnfrmDisbleDialog:false,
-		genExcptInMethod :true 
-	})
-}
-
-submitForm(formData){
-	let keywordData = Object.assign({},this.props.getAllKeywordData.data);
-
-    /*
-    * Here formData is handled to form value for keyowrd "genExcptInMethod"
-    *  genExcptInMethod = Perc %20 fqm %20 ExceptionType %20 ExceptionName
-    *  In fqm ,if string contains ';'.it has to be replaced with %3B
-    */
-    var genExcptInMethod=0
-    var length = Object.keys(formData).length
-    
-    /* below check handles the case of disabling the keyword i.e
-   	* when formData = {"genExcptInMethod":'0'}
-   	* there is no need to call constructVal function
-   	*/
-
-   	if(length > 1){
-   		genExcptInMethod = modifiedVal.constructVal(formData);
-   		keywordData.generateExceptionInMethod["value"] = genExcptInMethod ;
-   	}
-   	else
-   		keywordData.generateExceptionInMethod["value"] =  0 ;
-
-   	this.props.submitKeywordData(keywordData,this.props.profileId);
-   	
-   //action for runtime change
-   //triggerRunTimeChanges(trData,trModeDetail,formData);
-   let keywordDataList = [];
-   keywordDataList.push("generateExceptionInMethod"+ "=" + genExcptInMethod)
-   /*Object.keys(formData).forEach(function(key){
-   	keywordDataList.push(key + "=" + formData[key]); 
-   })  */  
-   triggerRunTimeChanges(this.props.trData, this.props.trModeDetail,keywordDataList); 
-   this.handleCancelGenExcptInMethod();
-}
+	handleCancelGenExcptInMethod() {
+		this.setState({ openEnableExcptCapturingDialog: false });
+	}
 
 
-render() {
-	const actions = [
-	<FlatButton
-	label="Cancel"
-	primary={true}
-	onTouchTap={this.handleCancelGenExcptInMethod.bind(this)}
-	/>,
-	<FlatButton
-	label="Submit"
-	primary={true}
-	keyboardFocused={true}
-	onClick={this.handleSubmitGenExcptInMethod.bind(this)}
-	/>
-	];
+	handleSubmitGenExcptInMethod() {
+		this.refs.enableGenExcptInMethod.submit();
+	}
 
+	handleRequestClose() {
+		this.setState({
+			openSnackBar: false
+		})
+	}
 
+	/*
+	* Disable Dialog functions
+	*/
+	cnfrmDisableExcptVal() {
+		var data = { 'generateExceptionInMethod': '0' }
+		this.submitForm(data);
+		this.setState({
+			openCnfrmDisbleDialog: false
+		})
 
+	}
 
-	const actionsDisable =[
-	<FlatButton
-	label="Cancel"
-	primary={true}
-	onTouchTap={this.handleCancelDisableExcptVal.bind(this)}
-	/>,
-	<FlatButton
-	label="OK"
-	primary={true}
-	keyboardFocused={true}
-	onClick={this.cnfrmDisableExcptVal.bind(this)}
-	/>
-	]
+	handleCancelDisableExcptVal() {
+		this.setState({
+			openCnfrmDisbleDialog: false,
+			genExcptInMethod: true
+		})
+	}
 
-	return (
-		<div style={{'paddingLeft':10,'paddingTop':10}}>
+	submitForm(formData) {
+		let keywordData = Object.assign({}, this.props.getAllKeywordData.data);
 
-		<div className = "row"  >
-		<div className = "col-md-5">
-		<Checkbox
-		value = "genExcptInMethod"
-		label = "Generate Exception In Method"
-		checked  = {this.state.genExcptInMethod}
-		onCustomChange={this.handleGenExcptInMethod.bind(this)}/>
-		<i style={{paddingLeft:40}}> Enable Exception generation in certain method</i>
-		</div>
-		<div>
-		<FlatButton className = "col-md-4" 
-					disabled ={!this.state.genExcptInMethod} 
-					onClick ={this.enableExcptCapturingDialog.bind(this)} 
-					label="Advanced Settings" />
-		</div>
-		</div>
+		/*
+		* Here formData is handled to form value for keyowrd "genExcptInMethod"
+		*  genExcptInMethod = Perc %20 fqm %20 ExceptionType %20 ExceptionName
+		*  In fqm ,if string contains ';'.it has to be replaced with %3B
+		*/
+		var genExcptInMethod = 0
+		var length = Object.keys(formData).length
 
+		/* below check handles the case of disabling the keyword i.e
+			  * when formData = {"genExcptInMethod":'0'}
+			  * there is no need to call constructVal function
+			  */
 
-		<DialogGenExceptInMethod
-		title = "Generate Exception In Method"
-		actions = {actions}
-		modal = {false}
-		open = {this.state.openEnableExcptCapturingDialog}
-		onRequestClose = {this.handleClose}
-		autoScrollBodyContent = {true}     
-		titleStyle={styles.title}    
-		>
-		<FormGenExcptInMethod ref="enableGenExcptInMethod" onSubmit ={this.submitForm.bind(this) } />
-		</DialogGenExceptInMethod>
+		if (length > 1) {
+			genExcptInMethod = modifiedVal.constructVal(formData);
+			keywordData.generateExceptionInMethod["value"] = genExcptInMethod;
+		}
+		else
+			keywordData.generateExceptionInMethod["value"] = 0;
 
+		this.props.submitKeywordData(keywordData, this.props.profileId);
 
+		//action for runtime change
+		//triggerRunTimeChanges(trData,trModeDetail,formData);
+		let keywordDataList = [];
+		keywordDataList.push("generateExceptionInMethod" + "=" + genExcptInMethod)
+		/*Object.keys(formData).forEach(function(key){
+			   keywordDataList.push(key + "=" + formData[key]); 
+		})  */
+		triggerRunTimeChanges(this.props.trData, this.props.trModeDetail, keywordDataList);
+		this.handleCancelGenExcptInMethod();
+	}
 
-		<Snackbar
-		open={this.state.openSnackBar}
-		message="Generate Exception In Method keywords with default values is enabled now."
-		autoHideDuration={4000}
-		onRequestClose={this.handleRequestClose.bind(this)}
-		/>
+	render() {
+		const actions = [
+			<FlatButton
+				label="Cancel"
+				primary={true}
+				onTouchTap={this.handleCancelGenExcptInMethod.bind(this)}
+				/>,
+			<FlatButton
+				label="Submit"
+				primary={true}
+				keyboardFocused={true}
+				onClick={this.handleSubmitGenExcptInMethod.bind(this)}
+				/>
+		];
 
-		<ConfirmDialog
-		title="Are you sure want to disable GenerateExceptionInMethod keyword ?"
-		actions={actionsDisable}
-		modal={false}
-		open={this.state.openCnfrmDisbleDialog}
-		>
-		</ConfirmDialog>
+		const actionsDisable = [
+			<FlatButton
+				label="Cancel"
+				primary={true}
+				onTouchTap={this.handleCancelDisableExcptVal.bind(this)}
+				/>,
+			<FlatButton
+				label="OK"
+				primary={true}
+				keyboardFocused={true}
+				onClick={this.cnfrmDisableExcptVal.bind(this)}
+				/>
+		]
 
+		return (
+			<div style={{ 'paddingLeft': 10, 'paddingTop': 10 }}>
 
-		</div>
+				<div className="row"  >
+					<div className="col-md-5">
+						<Checkbox
+							value="genExcptInMethod"
+							label="Generate Exception In Method"
+							checked={this.state.genExcptInMethod}
+							onCustomChange={this.handleGenExcptInMethod.bind(this)} />
+						<i style={{ paddingLeft: 40 }}> Enable Exception generation in certain method</i>
+					</div>
+					<div>
+
+						<RaisedButton
+							disabled={!this.state.genExcptInMethod}
+							onClick={this.enableExcptCapturingDialog.bind(this)}
+							style={{ width: 150 }}
+							backgroundColor="#18494F"
+							disabledLabelColor="#000"
+							labelColor="#FFF"
+							label="Advance Settings"
+							labelStyle={{ fontSize: 12 }} />
+
+					</div>
+				</div>
+
+				<DialogGenExceptInMethod
+					title="Generate Exception In Method"
+					actions={actions}
+					modal={false}
+					open={this.state.openEnableExcptCapturingDialog}
+					onRequestClose={this.handleClose}
+					autoScrollBodyContent={true}
+					titleStyle={styles.title}
+					>
+					<FormGenExcptInMethod ref="enableGenExcptInMethod" onSubmit={this.submitForm.bind(this)} />
+				</DialogGenExceptInMethod>
+
+				<Snackbar
+					open={this.state.openSnackBar}
+					message="Generate Exception In Method keywords with default values is enabled now."
+					autoHideDuration={4000}
+					onRequestClose={this.handleRequestClose.bind(this)}
+					/>
+
+				<ConfirmDialog
+					title="Are you sure want to disable GenerateExceptionInMethod keyword ?"
+					actions={actionsDisable}
+					modal={false}
+					open={this.state.openCnfrmDisbleDialog}
+					>
+				</ConfirmDialog>
+			</div>
 		);
 	}
 }
@@ -292,16 +282,14 @@ render() {
 
 function mapStateToProps(state) {
 	return {
-		getAllKeywordData :state.Keywords,
-		trData : state.initialData.trData,
+		getAllKeywordData: state.Keywords,
+		trData: state.initialData.trData,
 		trModeDetail: state.trModeDetail
 	};
 }
 
 //method to dispatch actions to the reducers
 function mapDispatchToProps(dispatch) {
-	//const actionMap = { loadInitTreeData: bindActionCreators(fetchTreeData, dispatch) };
-	//return actionMap;
 	return bindActionCreators(actionCreators, dispatch);
 }
-export default connect(mapStateToProps,mapDispatchToProps)(GenExcptInMethod);
+export default connect(mapStateToProps, mapDispatchToProps)(GenExcptInMethod);
--- src/containers/configuration/advance/putDelayInMethod/PutDelayInMethod.js
+++ src/containers/configuration/advance/putDelayInMethod/PutDelayInMethod.js
@@ -1,23 +1,24 @@
 //Importing React components
-import React,{PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { reduxForm } from 'redux-form';
 import TextField from 'material-ui/TextField';
 import RadioButton from 'material-ui/RadioButton';
-import FlatButton  from 'material-ui/FlatButton';
+import FlatButton from 'material-ui/FlatButton';
 import ConfirmDialog from 'material-ui/Dialog';
 import DialogPutDelayInMethod from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
 
 //Importing files
 import FormPutDelayInMethod from './Form_PutDelayInMethod';
 import Checkbox from '../../../../components/CheckboxWrapper';
 import RadioButtonGroup from '../../../../components/RadioButtonGroupWrapper';
-import {submitKeywordData,initializeInstrException}  from '../../../../actions/index';
-import {triggerRunTimeChanges} from '../../../../actions/runTimeChanges';
+import { submitKeywordData, initializeInstrException } from '../../../../actions/index';
+import { triggerRunTimeChanges } from '../../../../actions/runTimeChanges';
 import Snackbar from 'material-ui/Snackbar';
 
-export const fields = [ 'fromRange','toRange','isCpuHogg','isAutoInstrument','fqm'];
+export const fields = ['fromRange', 'toRange', 'isCpuHogg', 'isAutoInstrument', 'fqm'];
 
 const styles = {
   title: {
@@ -27,62 +28,60 @@ const styles = {
 }
 
 class PutDelayInMethod extends React.Component {
- 
+
   constructor(props) {
     super(props);
-    this.state = {putDelayInMethod:false}
-    this.state = {openPutDelayInMethodDialog : false}
+    this.state = { putDelayInMethod: false }
+    this.state = { openPutDelayInMethodDialog: false }
   }
 
 
-componentWillReceiveProps(nextProps)
-{
-    
-    if(this.props.initialData != nextProps.initialData){
-      if(nextProps.initialData != 0)
-      this.setState({putDelayInMethod : true
-      });
-    this.state = {openSnackBar:false}
-    }
-}
+  componentWillReceiveProps(nextProps) {
 
-handlePutDelayInMethod(event,isInputChecked){
-    if(isInputChecked === "false" || isInputChecked === false){
-            this.setState({openCnfrmDisbleDialog:true})
-    // this.setState({putDelayInMethod:isInputChecked})
-              
+    if (this.props.initialData != nextProps.initialData) {
+      if (nextProps.initialData != 0)
+        this.setState({
+          putDelayInMethod: true
+        });
+      this.state = { openSnackBar: false }
     }
+  }
 
+  handlePutDelayInMethod(event, isInputChecked) {
+    if (isInputChecked === "false" || isInputChecked === false) {
+      this.setState({ openCnfrmDisbleDialog: true })
+    }
     else
-       this.setState({putDelayInMethod : true,
-                      openSnackBar:true})
-    
+      this.setState({
+        putDelayInMethod: true,
+        openSnackBar: true
+      })
   }
 
-handleCancelDisablePutDelay(){
-     this.setState({putDelayInMethod:true,
-                    openCnfrmDisbleDialog:false
-     })
-  }
-
-cnfrmDisable(){
-
-    let keywordData = Object.assign({},this.props.getAllKeywordData.data);
-    keywordData["putDelayInMethod"]["value"] = 0 ;
-
-    this.props.submitKeywordData(keywordData,this.props.profileId); 
-    this.setState({putDelayInMethod:false,
-                    openCnfrmDisbleDialog:false
+  handleCancelDisablePutDelay() {
+    this.setState({
+      putDelayInMethod: true,
+      openCnfrmDisbleDialog: false
     })
-
   }
 
-enablePutDelayInMethodDialog(){
-    this.setState({openPutDelayInMethodDialog:true});
+  cnfrmDisable() {
+    let keywordData = Object.assign({}, this.props.getAllKeywordData.data);
+    keywordData["putDelayInMethod"]["value"] = 0;
+
+    this.props.submitKeywordData(keywordData, this.props.profileId);
+    this.setState({
+      putDelayInMethod: false,
+      openCnfrmDisbleDialog: false
+    })
   }
 
-submitForm(formData){
-  	let keywordData = Object.assign({},this.props.getAllKeywordData.data);
+  enablePutDelayInMethodDialog() {
+    this.setState({ openPutDelayInMethodDialog: true });
+  }
+
+  submitForm(formData) {
+    let keywordData = Object.assign({}, this.props.getAllKeywordData.data);
     var putDelayInMethod;
     /*
     * final data is data that is fetched from server and its value is updated according to user input,
@@ -91,113 +90,118 @@ submitForm(formData){
     *   putDelayInMethod = 5:20:0:1%20fqm for putDelayInMethod keyword
     */
 
-    putDelayInMethod = formData.fromRange+":";
+    putDelayInMethod = formData.fromRange + ":";
 
     /*if user does not entr the value of toRange,then
     * toRange = fromRange
     */
 
-    if(formData.toRange == null)
-      putDelayInMethod = putDelayInMethod +formData.fromRange+":";
+    if (formData.toRange == null)
+      putDelayInMethod = putDelayInMethod + formData.fromRange + ":";
     else
-      putDelayInMethod = putDelayInMethod +formData.toRange+":";
+      putDelayInMethod = putDelayInMethod + formData.toRange + ":";
 
-    if(formData.isCpuHogg === "true" || formData.isCpuHogg === true)
-      putDelayInMethod = putDelayInMethod +1+":";
+    if (formData.isCpuHogg === "true" || formData.isCpuHogg === true)
+      putDelayInMethod = putDelayInMethod + 1 + ":";
     else
-      putDelayInMethod = putDelayInMethod +0+":";
+      putDelayInMethod = putDelayInMethod + 0 + ":";
 
-    if(formData.isAutoInstrument === "true" || formData.isAutoInstrument === true)
-      putDelayInMethod = putDelayInMethod +1;
+    if (formData.isAutoInstrument === "true" || formData.isAutoInstrument === true)
+      putDelayInMethod = putDelayInMethod + 1;
     else
-      putDelayInMethod = putDelayInMethod +0;
+      putDelayInMethod = putDelayInMethod + 0;
 
-    if(formData.fqm != null)
-      putDelayInMethod = putDelayInMethod +"%20"+formData.fqm;
-           
-      keywordData.putDelayInMethod["value"] = putDelayInMethod;
+    if (formData.fqm != null)
+      putDelayInMethod = putDelayInMethod + "%20" + formData.fqm;
 
-    this.props.submitKeywordData(keywordData,this.props.profileId); 
+    keywordData.putDelayInMethod["value"] = putDelayInMethod;
+
+    this.props.submitKeywordData(keywordData, this.props.profileId);
 
     //action for runtime change
-   //triggerRunTimeChanges(trData,trModeDetail,formData);
-   let keywordDataList = [];
-   keywordDataList.push("putDelayInMethod" + "=" + putDelayInMethod);   
-   triggerRunTimeChanges(this.props.trData, this.props.trModeDetail,keywordDataList); 
-   this.handleCancelPutDelayInMethod();
+    //triggerRunTimeChanges(trData,trModeDetail,formData);
+    let keywordDataList = [];
+    keywordDataList.push("putDelayInMethod" + "=" + putDelayInMethod);
+    triggerRunTimeChanges(this.props.trData, this.props.trModeDetail, keywordDataList);
+    this.handleCancelPutDelayInMethod();
   }
 
-handleSubmitPutDelayInMethod(){
-  this.refs.putDelayInMethodForm.submit();
- 
+  handleSubmitPutDelayInMethod() {
+    this.refs.putDelayInMethodForm.submit();
+
   }
 
-handleCancelPutDelayInMethod(){
+  handleCancelPutDelayInMethod() {
     // this.props.toggleStateDialogEditTopo();
-     this.setState({openPutDelayInMethodDialog:false});
+    this.setState({ openPutDelayInMethodDialog: false });
   }
 
-handleCancelDisablePutDelay(){
-  this.setState({ openCnfrmDisbleDialog:false,
-                  enableBCICheckBox :true 
-   })
-}
+  handleCancelDisablePutDelay() {
+    this.setState({
+      openCnfrmDisbleDialog: false,
+      enableBCICheckBox: true
+    })
+  }
 
-handleRequestClose(){
-  this.setState({openSnackBar:false
-  })
-}
-render() {
+  handleRequestClose() {
+    this.setState({
+      openSnackBar: false
+    })
+  }
+  render() {
 
-const actionsPutDelayDisable =[
-        <FlatButton
+    const actionsPutDelayDisable = [
+      <FlatButton
         label="Cancel"
         primary={true}
         onTouchTap={this.handleCancelDisablePutDelay.bind(this)}
-      />,
+        />,
       <FlatButton
         label="OK"
         primary={true}
         keyboardFocused={true}
         onClick={this.cnfrmDisable.bind(this)}
-      />
-];
+        />
+    ];
 
-const actions =[
-        <FlatButton
+    const actions = [
+      <FlatButton
         label="Cancel"
         primary={true}
         onTouchTap={this.handleCancelPutDelayInMethod.bind(this)}
-      />,
+        />,
       <FlatButton
         label="Submit"
         primary={true}
         keyboardFocused={true}
         onClick={this.handleSubmitPutDelayInMethod.bind(this)}
-      />
-]
+        />
+    ];
 
-
-return (
+    return (
       <div  >
-     
-      	<div className = "row" style={{'paddingLeft':10}}>
-         <div className = "col-md-5" >
-      		 <Checkbox
+        <div className="row" style={{ 'paddingLeft': 10 }}>
+          <div className="col-md-5" >
+            <Checkbox
               label="Put Delay in Method"
               value="putDelayInMethod"
-              checked  = {this.state.putDelayInMethod}
-              onCustomChange={this.handlePutDelayInMethod.bind(this)}    />
-            <i style={{paddingLeft:40}}>Add delay in any specified method using instrumentation</i>
-            </div>
-          
-         <div>
-         <FlatButton   className = "col-md-4" 
-                      disabled ={!this.state.putDelayInMethod} 
-                      onClick ={this.enablePutDelayInMethodDialog.bind(this)} 
-                      label="Advanced Settings" />
-         </div>
-        </div> 
+              checked={this.state.putDelayInMethod}
+              onCustomChange={this.handlePutDelayInMethod.bind(this)} />
+            <i style={{ paddingLeft: 40 }}>Add delay in any specified method using instrumentation</i>
+          </div>
+
+          <div>
+            <RaisedButton
+              disabled={!this.state.putDelayInMethod}
+              onClick={this.enablePutDelayInMethodDialog.bind(this)}
+              style={{ width: 150 }}
+              backgroundColor="#18494F"
+              disabledLabelColor="#000"
+              labelColor="#FFF"
+              label="Advance Settings"
+              labelStyle={{ fontSize: 12 }} />
+          </div>
+        </div>
 
         <DialogPutDelayInMethod
           title="Put Delay in Method"
@@ -205,25 +209,25 @@ return (
           modal={false}
           open={this.state.openPutDelayInMethodDialog}
           onRequestClose={this.handleClose}
-          autoScrollBodyContent={true}     
-          titleStyle={styles.title}    
-        >
-        <FormPutDelayInMethod ref="putDelayInMethodForm" onSubmit ={this.submitForm.bind(this) } />
-       </DialogPutDelayInMethod>
+          autoScrollBodyContent={true}
+          titleStyle={styles.title}
+          >
+          <FormPutDelayInMethod ref="putDelayInMethodForm" onSubmit={this.submitForm.bind(this)} />
+        </DialogPutDelayInMethod>
 
         <Snackbar
           open={this.state.openSnackBar}
           message="PutDelayInMethod keyword with its default value is enabled now."
           autoHideDuration={4000}
           onRequestClose={this.handleRequestClose.bind(this)}
-        />
-    	
-       <ConfirmDialog
+          />
+
+        <ConfirmDialog
           title="Are you sure want to disable the applied settings?"
           actions={actionsPutDelayDisable}
           modal={false}
           open={this.state.openCnfrmDisbleDialog}
-        >
+          >
         </ConfirmDialog>
       </div>
     );
@@ -241,14 +245,14 @@ export default reduxForm({
   fields
 },
   state => ({ // mapStateToProps
-  getAllKeywordData :state.Keywords,
-  initialData :state.Keywords.initializeKeywords.putDelayInMethodObj,
-  initialValues :state.Keywords.initializeKeywords.putDelayInMethodObj,
-  trData : state.initialData.trData,
-  trModeDetail: state.trModeDetail
-}),
-  
-  { 
-   submitKeywordData:submitKeywordData,
- } // mapDispatchToProps (will bind action creator to dispatch)
+    getAllKeywordData: state.Keywords,
+    initialData: state.Keywords.initializeKeywords.putDelayInMethodObj,
+    initialValues: state.Keywords.initializeKeywords.putDelayInMethodObj,
+    trData: state.initialData.trData,
+    trModeDetail: state.trModeDetail
+  }),
+
+  {
+    submitKeywordData: submitKeywordData,
+  } // mapDispatchToProps (will bind action creator to dispatch)
 )(PutDelayInMethod);
